821a1fbc3552d8df2092d45e435eced10aead7b0
Solomon Foster <colomon@gmail.com>
2014-10-15 15:21:36 -0400

    Refactor TuneIndex stuff into TuneIndex.pm.

--- bin/make-txt-index.pl
+++ bin/make-txt-index.pl
@@ -1,77 +1,8 @@
 use v6;
-# use Grammar::Tracer;
+use TuneIndex;
 
-sub infix:<++> ($a, $b) {
-    my @ = $a.values, $b.values;
-}
-
-class TuneIndex {
-    has Rat $.index;
-    has Str $.name;
-    has %.info;
-    
-    method names() {
-        self.name ++ self.info<AKA> ++ self.info<GIVEN>;
-    }
-}
-
-grammar TuneIndex::Grammar {
-    rule TOP {
-        <tune>+
-    }
-    
-    rule tune {
-        <index> <name>
-        <info>* % \v
-    }
-    
-    token index {
-        \d+ '.' \d
-    }
-    
-    token name {
-        \V+ $$
-    }
-    
-    token info {
-        <key> <.ws> <name>
-    }
-    
-    token key {
-        < C GIVEN AKA NOTE >
-    }
-}
-
-my %tunes;
-
-class TuneIndex::Actions {
-    method tune($/) {
-        my %info;
-        for $<info>.list -> $i {
-            %info{$i.ast.key}.push($i.ast.value);
-        }
-        
-        my $index = $<index>.ast;
-        die "Tune $index already exists" if %tunes{$index}:exists;
-        %tunes{$index} = TuneIndex.new(:$index,
-                                       name => $<name>.ast,
-                                       info => %info);
-    }
-    
-    method index($/) {
-        make (~$/).Rat
-    }
-    
-    method info($/) {
-        make ~$<key> => $<name>.ast
-    }
-    
-    method name($/) {
-        make $/.chomp
-    }
-}
-
-TuneIndex::Grammar.parse(slurp("trip-to-sligo"), :actions(TuneIndex::Actions.new)) or die "Unable to parse trip-to-sligo";
+my $tunes = TuneIndex::Actions.new;
+TuneIndex::Grammar.parse(slurp("trip-to-sligo"), :actions($tunes)) or die "Unable to parse trip-to-sligo";
 
 sub sortable-name($_) {
     when /^The \s+ (.*)$/ { $0 ~ ", The" }
@@ -79,7 +10,7 @@ sub sortable-name($_) {
 }
 
 my %names;
-for %tunes.kv -> $index, $tune {
+for $tunes.tunes.kv -> $index, $tune {
     for $tune.names -> $name {
         %names{sortable-name($name)}.push($index);
     }
---
+++ lib/TuneIndex.pm
@@ -0,0 +1,75 @@
+use v6;
+
+sub infix:<++> ($a, $b) {
+    my @ = $a.values, $b.values;
+}
+
+class TuneIndex {
+    has Rat $.index;
+    has Str $.name;
+    has %.info;
+    
+    method names() {
+        self.name ++ self.info<AKA> ++ self.info<GIVEN>;
+    }
+
+    method composer() {
+        self.info<C> ?? self.info<C>.join(", ") !! Str;
+    }
+}
+
+grammar TuneIndex::Grammar {
+    rule TOP {
+        <tune>+
+    }
+    
+    rule tune {
+        <index> <name>
+        <info>* % \v
+    }
+    
+    token index {
+        \d+ '.' \d
+    }
+    
+    token name {
+        \V+ $$
+    }
+    
+    token info {
+        <key> <.ws> <name>
+    }
+    
+    token key {
+        < C GIVEN AKA NOTE >
+    }
+}
+
+class TuneIndex::Actions {
+    has %.tunes;
+    
+    method tune($/) {
+        my %info;
+        for $<info>.list -> $i {
+            %info{$i.ast.key}.push($i.ast.value);
+        }
+        
+        my $index = $<index>.ast;
+        die "Tune $index already exists" if %.tunes{$index}:exists;
+        %.tunes{$index} = TuneIndex.new(:$index,
+                                        name => $<name>.ast,
+                                        info => %info);
+    }
+    
+    method index($/) {
+        make (~$/).Rat
+    }
+    
+    method info($/) {
+        make ~$<key> => $<name>.ast
+    }
+    
+    method name($/) {
+        make $/.chomp
+    }
+}
